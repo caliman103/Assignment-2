6189bcf4530171eb179b237da7e1d2321c4baa2b
Bigvinn23 <46977904+Bigvinn23@users.noreply.github.com>
2022-03-16 10:43:46 -0400

    changed back the name

--- Animal.java
+++ Animal.java
@@ -1,6 +1,6 @@
 /*
 ClassName: Animal
-Author: Jamaine Drakes, Evan
+Author: Jamaine Drakes, Evan Leacock
 Purpose: The puspose of this class is to manage information about each animal in the Zoo
 Start Date: Mar 10, 2022
 Last Edit: Mar 11, 2022
